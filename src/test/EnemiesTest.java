bfe8e973f8f962f7938457d2f42a078e25bf2d56
z5312886 <p.jagtap@student.unsw.edu.au>
2021-07-13 22:16:48 +1000

    Updated enemy support behaviour

--- src/test/EnemiesTest.java
+++ src/test/EnemiesTest.java
@@ -24,17 +24,12 @@ class EnemiesTest {
     private int moveDown = 1;
     private int zombieReach = 2;
     private int vampireReach = 3;
-    private int slugPosition = 2;
-    private int zombiePosition = 4;
-    private int vampirePosition = 6;
-    private Slug newSlug;
-    private Zombie newZombie;
-    private Vampire newVampire;
+    private int characterPosition = 0;
     private Character newCharacter;
     private LoopManiaWorld testWorld;
     private List<Pair<Integer, Integer>> orderedPath = new ArrayList<>();
 
-    /**
+    /** TODO: add assumption to assumptions file
      * Checks Made Assumption:
      * When enemies are spawned, they will move x blocks up then return to spawning position 
      * then move x blocks down, then return back to the spawning position and so on. 
@@ -43,12 +38,14 @@ class EnemiesTest {
      * Slug: x = 1
      * Zombie: x = 2
      * Vampire: x = 3
+     * 
+     * Note: multiple enemies can momentarily exist on one tile
+     * when their paths are crossing each other
      */
     @Test
     void testCharacterMovement() {
         initializeWorld();
 
-        int characterPosition = 0;
         int posX = newCharacter.getX();
         int posY = newCharacter.getY(); 
         assertEquals(orderedPath.get(characterPosition), Pair.with(posX, posY));
@@ -56,18 +53,18 @@ class EnemiesTest {
         // tick the world
         testWorld.runTickMoves();
 
-        // character should move down
+        // character should move clockwise
         posX = newCharacter.getX();
         posY = newCharacter.getY(); 
-        assertEquals(orderedPath.get(characterPosition + moveDown), Pair.with(posX, posY));
+        assertEquals(Pair.with(0, 1), Pair.with(posX, posY));
 
         // tick the world
         testWorld.runTickMoves();
 
-        // character should keep moving down
+        // character should keep moving clockwise
         posX = newCharacter.getX();
         posY = newCharacter.getY(); 
-        assertEquals(orderedPath.get(characterPosition + moveDown * 2), Pair.with(posX, posY));
+        assertEquals(Pair.with(0, 2), Pair.with(posX, posY));
 
         // tick the world
         testWorld.runTickMoves();
@@ -75,12 +72,19 @@ class EnemiesTest {
         // character should change direction
         posX = newCharacter.getX();
         posY = newCharacter.getY(); 
-        assertEquals(orderedPath.get(characterPosition + moveDown * 3), Pair.with(posX, posY));
+        assertEquals(Pair.with(1, 2), Pair.with(posX, posY));
     }
 
     @Test
     void testSlugMovement() {
         initializeWorld();
+
+        int slugPosition = 2;
+
+        // initializing slug
+        PathPosition slugPathPosition = new PathPosition(slugPosition, orderedPath);
+        Slug newSlug = new Slug(slugPathPosition);
+        testWorld.addEntity(newSlug);
 
         // check if initial location is correct
         int posX = newSlug.getX();
@@ -118,11 +122,33 @@ class EnemiesTest {
         posX = newSlug.getX();
         posY = newSlug.getY(); 
         assertEquals(orderedPath.get(slugPosition), Pair.with(posX, posY));
+
+        // check edge case at P(0, 0)
+
+        // initializing another Slug
+        PathPosition anotherSlugPath = new PathPosition(0, orderedPath);
+        Slug anotherSlug = new Slug(anotherSlugPath);
+        testWorld.addEntity(anotherSlug);
+
+        // tick the world
+        testWorld.runTickMoves();
+
+        // position should wrap around the array when moving up
+        posX = anotherSlug.getX();
+        posY = anotherSlug.getY(); 
+        assertEquals(Pair.with(0, 1), Pair.with(posX, posY));
     }
 
     @Test
     void testZombieMovement() {
         initializeWorld();
+
+        int zombiePosition = 4;
+
+        // initialize zombie
+        PathPosition zombiePathPosition = new PathPosition(zombiePosition, orderedPath);
+        Zombie newZombie = new Zombie(zombiePathPosition);
+        testWorld.addEntity(newZombie);
 
         // check if initial location is correct
         int posX = newZombie.getX();
@@ -164,11 +190,34 @@ class EnemiesTest {
         posX = newZombie.getX();
         posY = newZombie.getY(); 
         assertEquals(orderedPath.get(zombiePosition), Pair.with(posX, posY));
+
+        // check edge case at P(0, 0)
+
+        // initializing another Zombie
+        PathPosition newPathPosition = new PathPosition(0, orderedPath);
+        Zombie anotherZombie = new Zombie(newPathPosition);
+        testWorld.addEntity(anotherZombie);
+
+        // tick the world
+        testWorld.runTickMoves();
+        testWorld.runTickMoves();
+
+        // position should wrap around the array when moving up
+        posX = anotherZombie.getX();
+        posY = anotherZombie.getY(); 
+        assertEquals(Pair.with(0, 2), Pair.with(posX, posY));
     }
 
     @Test
     void testVampireMovement() {
         initializeWorld();
+
+        int vampirePosition = 6;
+
+        // initialize vampire
+        PathPosition vampirePathPosition = new PathPosition(vampirePosition, orderedPath);
+        Vampire newVampire = new Vampire(vampirePathPosition);
+        testWorld.addEntity(newVampire);
 
         // check if initial location is correct
         int posX = newVampire.getX();
@@ -214,29 +263,128 @@ class EnemiesTest {
         posX = newVampire.getX();
         posY = newVampire.getY(); 
         assertEquals(orderedPath.get(vampirePosition), Pair.with(posX, posY));
+
+        // check edge case at P(0, 0)
+
+        // initializing another Vampire
+        PathPosition newPathPosition = new PathPosition(0, orderedPath);
+        Vampire anotherVampire = new Vampire(newPathPosition);
+        testWorld.addEntity(anotherVampire);
+
+        // tick the world
+        testWorld.runTickMoves();
+        testWorld.runTickMoves();
+        testWorld.runTickMoves();
+
+        // position should wrap around the array when moving up
+        posX = anotherVampire.getX();
+        posY = anotherVampire.getY(); 
+        assertEquals(Pair.with(1, 2), Pair.with(posX, posY));
     }
 
     /**
      * When some enemy is within the support radius of Slug,
-     * check if Slug goes to help
+     * check if Slug helps
+     * 
+     * TODO: Add assumption to assumptions file
+     * Checks Assumption:
+     * 
+     * When some enemy X is within the support radius of another
+     * enemy Y, the enemy Y teleports to X's position + 1 and the
+     * battle state should calculate the the total damage dealt on MC
+     * 
+     * Note: such teleportation only occurs when the enemy is in battle
      */
     @Test
     void testSlugSupportRadius() {
         initializeWorld();
+
+        int slugPosition = 2;
+        int anotherSlugPosition = slugPosition + 1;
+
+        // initializing slug
+        PathPosition slugPathPosition = new PathPosition(slugPosition, orderedPath);
+        Slug newSlug = new Slug(slugPathPosition);
+        testWorld.addEntity(newSlug);
+
+        // initializing another slug which is next to the original slug
+        PathPosition anotherSlugPath = new PathPosition(anotherSlugPosition, orderedPath);
+        Slug slugTwo = new Slug(anotherSlugPath);
+        testWorld.addEntity(slugTwo);
+
+        // tick world
+        testWorld.runTickMoves();
+
+        // since a Slug has a support radius 1, the Slugs should just keep moving
+        // should move up first
+        int posXOne = newSlug.getX();
+        int posYOne = newSlug.getY(); 
+        assertEquals(orderedPath.get(slugPosition + moveUp), Pair.with(posXOne, posYOne));
+
+        // should move up first
+        int posXTwo = slugTwo.getX();
+        int posYTwo = slugTwo.getY(); 
+        assertEquals(orderedPath.get(anotherSlugPosition + moveUp), Pair.with(posXTwo, posYTwo));   
     }
 
     /**
      * When some enemy is within the support radius of Zombie,
-     * check if Zombie goes to help
+     * check if Zombie goes to help (given that battle is initiated)
      */
     @Test
     void testZombieSupportRadius() {
         initializeWorld();
+
+        int mcPosition = 6;
+        int slugPosition = 5;
+        int zombiePosition = 3;
+        int expectedPosition = 4;
+
+        // tick the world to place character in position
+        testWorld.runTickMoves();
+        testWorld.runTickMoves();
+
+        // ensure that MC is at expected location
+        int locX = newCharacter.getX();
+        int locY = newCharacter.getY();
+        assertEquals(orderedPath.get(mcPosition), Pair.with(locX, locY));
+
+        // initialize an enemy Slug next to the character
+        PathPosition slugPathPosition = new PathPosition(slugPosition, orderedPath);
+        Slug newSlug = new Slug(slugPathPosition);
+        testWorld.addEntity(newSlug);
+
+        /**
+         * initialize a Zombie such that its position is within radius 2 of slug
+         * 
+         * ensure that Zombie is located before Slug so we can check if the teleportation
+         * 
+         * takes place as expected or else movement can be attributed to the tick() outcome
+         */
+        PathPosition zombiePathPosition = new PathPosition(zombiePosition, orderedPath);
+        Zombie newZombie = new Zombie(zombiePathPosition);
+        testWorld.addEntity(newZombie);
+
+        // tick the world
+        testWorld.runTickMoves();
+
+        /**
+         * check if the new location of Zombie is next to the Slug
+         * 
+         * note: the Zombie moves next to the Slug because the Slug is 
+         * engaged in battle with the main character
+         * 
+         * note: 'engaged' in battle means MC is within the battle
+         * radius of an enemy
+         */
+        locX = newZombie.getX();
+        locY = newZombie.getY();
+        assertEquals(orderedPath.get(expectedPosition), Pair.with(locX, locY));
     }
 
     /**
      * When some enemy is within the support radius of Vampire,
-     * check if Vampire goes to help
+     * check if Vampire goes to help (given that battle is initiated)
      */
     @Test
     void testVampireSupportRadius() {
@@ -267,7 +415,7 @@ class EnemiesTest {
     // setup template world
     public void initializeWorld() {
         int LOOP_SIZE = 3;
-        int characterPosition = 0;
+
         // setting world path
         orderedPath.add(Pair.with(0, 0));
         orderedPath.add(Pair.with(1, 0));
@@ -283,20 +431,5 @@ class EnemiesTest {
         PathPosition characterPathPosition = new PathPosition(characterPosition, orderedPath);
         newCharacter = new Character(characterPathPosition);
         testWorld.setCharacter(newCharacter);
-        
-        // initializing slug
-        PathPosition slugPathPosition = new PathPosition(slugPosition, orderedPath);
-        Slug newSlug = new Slug(slugPathPosition);
-        testWorld.addEntity(newSlug);
-        
-        // initialize zombie
-        PathPosition zombiePathPosition = new PathPosition(zombiePosition, orderedPath);
-        Zombie newZombie = new Zombie(zombiePathPosition);
-        testWorld.addEntity(newZombie);
-
-        // initialize vampire
-        PathPosition vampirePathPosition = new PathPosition(vampirePosition, orderedPath);
-        Vampire newVampire = new Vampire(vampirePathPosition);
-        testWorld.addEntity(newVampire);
     }
 }
