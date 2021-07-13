76176f7b04ca866573342c938d763bedebde0928
Jason Yu <jasonyu0100@gmail.com>
2021-07-13 19:54:30 +1000

    adding building class, and updating onLoad Code

--- src/unsw/loopmania/buildings/BarracksBuilding.java
+++ src/unsw/loopmania/buildings/BarracksBuilding.java
@@ -1,12 +1,11 @@
 package unsw.loopmania.buildings;
 
 import javafx.beans.property.SimpleIntegerProperty;
-import unsw.loopmania.StaticEntity;
 
 /**
  * a basic form of building in the world
  */
-public class BarracksBuilding extends StaticEntity {
+public class BarracksBuilding extends Building {
     // TODO = add more types of building, and make sure buildings have effects on entities as required by the spec
     public BarracksBuilding(SimpleIntegerProperty x, SimpleIntegerProperty y) {
         super(x, y);
