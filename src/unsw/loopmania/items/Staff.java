95cf06fe57af591fcb7100d3354186016820e597
z5312886 <p.jagtap@student.unsw.edu.au>
2021-07-14 16:34:49 +1000

    Added functionality for shop test

--- src/unsw/loopmania/items/Staff.java
+++ src/unsw/loopmania/items/Staff.java
@@ -1,13 +1,16 @@
 package unsw.loopmania.items;
 
 import javafx.beans.property.SimpleIntegerProperty;
-import unsw.loopmania.StaticEntity;
 
 /**
  * represents an equipped or unequipped staff in the backend world
  */
-public class Staff extends StaticEntity {
+public class Staff extends AttackItem {
+    private static int damage = 3;
+    private static int itemCost = 8;
+    private static int itemDurability = 8;
+
     public Staff(SimpleIntegerProperty x, SimpleIntegerProperty y) {
-        super(x, y);
+        super(x, y, itemCost, itemDurability, damage);
     }    
 }
