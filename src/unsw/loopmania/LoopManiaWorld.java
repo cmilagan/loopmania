22a18dc05236de676f83f9868168138a0a356bbc
z5312886 <p.jagtap@student.unsw.edu.au>
2021-07-15 19:09:37 +1000

    removed observer pattern comment

--- src/unsw/loopmania/LoopManiaWorld.java
+++ src/unsw/loopmania/LoopManiaWorld.java
@@ -388,8 +388,6 @@ public class LoopManiaWorld {
 
     /**
      * move all enemies
-     * 
-     * TODO: Use observer pattern here
      */
     private void moveBasicEnemies() {
         // TODO = expand to more types of enemy
