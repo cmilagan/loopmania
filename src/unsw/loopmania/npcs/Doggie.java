e36a5d439ee7cf7a23bbe0c74950433e09c91700
subrat shrestha <z5266273@ad.unsw.edu.au>
2021-07-28 19:59:36 +1000

    basic doggie

---
+++ src/unsw/loopmania/npcs/Doggie.java
@@ -0,0 +1,38 @@
+package unsw.loopmania.npcs;
+
+import unsw.loopmania.PathPosition;
+
+public class Doggie extends BasicEnemy {
+    public Doggie(PathPosition position) {
+        super(position, 0, 20, 100, 1, 1);
+    }
+
+    private int distanceTravelled = 0;
+    private boolean travellingUpwards = true;
+
+    /**
+     * Movement similar to Slug.
+     */
+    @Override
+    public void move() {
+        distanceTravelled++;
+
+        // check if Doggie is at either bound
+        if ((distanceTravelled % 2) == 0) {
+            if (travellingUpwards) {
+                travellingUpwards = false;
+                moveDownPath();
+            } else {
+                travellingUpwards = true;
+                moveUpPath();
+            }
+        } else {
+            // continue with existing path
+            if (travellingUpwards) {
+                moveUpPath();
+            } else {
+                moveDownPath();
+            }
+        }
+    }
+}
